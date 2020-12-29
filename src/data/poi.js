c9227186c20ec509022e3dc093dc9ce4653ad5f0
guilhemmonnier <guilhem.monnier0105@gmail.com>
2020-12-29 16:36:13 +0100

    Code source et données de la carte interactive

---
+++ src/data/poi.js
@@ -0,0 +1,37 @@
+points =  [{
+	
+	"fields": {
+		"nom": "Patinoire olympique d'Amnéville",
+		"wgs84": [49.244248,6.134576],
+		"description": " Le royaume de la glisse pour tous les âges ! Les adolescents s'y retrouvent et les adultes se (re) découvrent des dons de patineurs !",
+		"adresse": " Rue du tigre la cité des loisirs, 57360 Amnéville",
+		"telephone": "+33 3 87 71 11 26",
+		"longitude": 6.134576,
+		"latitude": 49.244248,
+		"categorie1": "Sport et loisir",
+		"categorie3": "Patin à glace",
+		"categorie2": "Commerce Local"
+	},
+	"geometry": {
+		"type": "Point",
+		"coordinates": [49.244248,6.134576]
+	}
+}, {
+
+	"fields": {
+		"nom": "Eglise Saint Léopold",
+		"wgs84": [48.5991891689009,6.48766279220581],
+		"description": " L'actuelle église Saint-Léopold fut édifiée de 1953 à  1954 sous la responsabilité du père Aubry, grand amateur d'art contemporain.",
+		"adresse": " 4 Rue Viox,54300 Lunéville",
+		"telephone": "+33 3 83 73 04 59",
+		"longitude": 6.48766279220581,
+		"latitude": 48.5991891689009,
+		"categorie1": "Culture",
+		"categorie3": "Culte",
+		"categorie2": ""
+	},
+	"geometry": {
+		"type": "Point",
+		"coordinates": [6.48766279220581, 48.5991891689009]
+	}
+}]
