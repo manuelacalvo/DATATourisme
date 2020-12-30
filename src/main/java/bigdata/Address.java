1031ddff3b5ad7a25eda948bf1b8a13e6b73a341
guilhemmonnier <guilhem.monnier0105@gmail.com>
2020-12-30 19:54:21 +0100

    Requetes hive depuis app Java et conversion de csv à objet

---
+++ src/main/java/bigdata/Address.java
@@ -0,0 +1,18 @@
+package bigdata;
+
+public class Address {
+	public String locality;
+    public String postalCode;
+    public String street;
+
+    @Override
+    public String toString() {
+        return this.locality + "," + this.postalCode + "," + this.street;
+    }
+    
+    public Address(String locality, String postalCode, String street) {
+    	this.locality = locality;
+    	this.postalCode = postalCode;
+    	this.street = street;
+    }
+}
---
+++ src/main/java/bigdata/POI.java
@@ -0,0 +1,51 @@
+package bigdata;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class POI {
+    public String id;
+    public String name;
+    public List<String> type = new ArrayList<>();
+    public String comment;
+    public String lastUpdate;
+    public String reduceMobilityAccess;
+    public Address address;
+    public String latitude;
+    public String longitude;
+
+    
+    public POI(String id, String name, List<String> type, String comment, String lastUpdate, String reduceMobilityAccess, Address address, String latitude, String longitude) {
+    	this.id = id;
+    	this.name = name;
+    	this.type = type;
+    	this.comment = comment;
+    	this.lastUpdate = lastUpdate;
+    	this.reduceMobilityAccess = reduceMobilityAccess;
+    	this.address = address;
+    	this.latitude = latitude;
+    	this.longitude = longitude;
+    	}
+    @Override
+    public String toString() {
+        return this.id + "," + this.name + "," + toStringArray(this.type) + "," + this.comment + "," + this.lastUpdate + "," + this.reduceMobilityAccess
+                + "," + this.address.toString() + "," + this.latitude + "," + this.longitude + "\n";
+    }
+
+    public String toStringArray(List<String> list) {
+        if (list != null && list.size()>0) {
+            final String[] str = {""};
+
+            list.forEach(current -> str[0] += current + "|");
+            return str[0];
+        } else return null;
+    }
+    
+    public String getType() {
+    	return toStringArray(this.type);
+    }
+
+}
+
+
